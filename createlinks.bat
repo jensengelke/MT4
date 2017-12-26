0d74ee348032e20575acf8b721638c210710b94a
jensengelke <jens.engelke@gmail.com>
2017-12-26 10:23:47 +0100

    refactoring

--- createlinks.bat
+++ createlinks.bat
@@ -1,8 +1,4 @@
-rmdir experts
-rmdir include
-rmdir indicators
-rmdir presets
-mklink /j Experts "D:\GitHub\MT4\mql5\experts"
-mklink /j Include "d:\GitHub\MT4\mql5\Include"
-mklink /j Indicators "d:\GitHub\MT4\mql5\Indicators"
-mklink /j Presets "d:\GitHub\MT4\mql5\Presets"
+mklink /j Experts\MyExperts "c:\GitHub\MT4\experts"
+mklink /j Include\MyInclude "c:\GitHub\MT4\MyInclude"
+mklink /j Indicators\MyIndicators "c:\GitHub\MT4\MyIndicators"
+mklink /j Presets\MyPresets "c:\GitHub\MT4\MyPresets"
