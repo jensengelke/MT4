da616394c0bc68cd4a74d1b393e9890febc8a4a9
jensengelke <jens.engelke@gmail.com>
2017-12-26 10:29:58 +0100

    correct link creation for experts
    correct link creation for experts

--- createlinks.bat
+++ createlinks.bat
@@ -1,4 +1,4 @@
-mklink /j Experts\MyExperts "c:\GitHub\MT4\experts"
+mklink /j Experts\MyExperts "c:\GitHub\MT4\MyExperts"
 mklink /j Include\MyInclude "c:\GitHub\MT4\MyInclude"
 mklink /j Indicators\MyIndicators "c:\GitHub\MT4\MyIndicators"
 mklink /j Presets\MyPresets "c:\GitHub\MT4\MyPresets"
